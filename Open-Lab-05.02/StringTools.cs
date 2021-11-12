a9fd1c44169694937c187255c1f0cd2d9c45a85b
Majinoo <91146809+Majinoo@users.noreply.github.com>
2021-11-12 10:23:33 +0100

    I implemented the method that transforms a sentence that ends with multiple question marks ? or multiple exclamation marks ! into a sentence that ends only with one. Run with CTRL+F5 to test it.

--- Open-Lab-05.02/StringTools.cs
+++ Open-Lab-05.02/StringTools.cs
@@ -1,12 +1,43 @@
-﻿using System;
-
-namespace Open_Lab_05._02
+﻿namespace Open_Lab_05._02
 {
     public class StringTools
     {
         public string NoYelling(string sentence)
         {
-            throw new NotImplementedException();
+            if (sentence.EndsWith("??"))
+            {
+                for (int i = sentence.Length - 1; i >= 0; i--)
+                {
+                    if (sentence[i].ToString() == "?")
+                    {
+                        sentence = sentence.Remove(i, 1);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                return sentence + "?";
+            }
+            else if (sentence.EndsWith("!!"))
+            {
+                for (int i = sentence.Length - 1; i >= 0; i--)
+                {
+                    if (sentence[i].ToString() == "!")
+                    {
+                        sentence = sentence.Remove(i, 1);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                return sentence + "!";
+            }
+            else
+            {
+                return sentence;
+            }
         }
     }
 }
